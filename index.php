7c473bede8ac985fb6455bd98f2408b3a4c0eefa
Nikicoraz <nicolacorato05@gmail.com>
2023-11-21 21:53:01 +0100

    Programming languages

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nikicoraz</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Welcome</h1>
     <div id="bio">
-        My name is Nicola, also known as Nikicoraz. I'm an italian programmer and I <em>sometimes</em> do stuff. Also Arch Linux enjoyer.
+        <p>
+            My name is Nicola, also known as Nikicoraz. I'm an italian programmer and I <em>sometimes</em> do stuff.
+        </p>
+        <p>I've used a lot of <a href="programming.html">programming languages</a>, but the ones I used the most are C, C#, Java, Python, HTML, CSS, JavaScript.</p>
+        <p>Check out my <a href="https://github.com/Nikicoraz">Github</a> for a lot of <em>(unfinished)</em> projects ;)</p>
+        <p>Also I use arch btw.</p>
     </div>
     <h2>Some random pages that I have made:</h2>
     <?php
@@ -17,7 +21,7 @@
 
     $files = scandir(".");  
     foreach ($files as $file) {
-        if(!is_file($file) && $file != "." && $file != ".."){
+        if(!is_file($file) && $file != "." && $file != ".." && $file != ".git"){
             echo "<a href='" . $file . "'>" . $file ."</a><br>";
         }
     }
